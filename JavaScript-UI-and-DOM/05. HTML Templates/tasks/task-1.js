0050d17dd20453fb8f60efe7eeb84493c833b0d6
Milena Shishkova <shishkova.milena@gmail.com>
2017-03-23 20:59:23 +0200

    Added Task1 of handlebars.

--- JavaScript-UI-and-DOM/05. HTML Templates/tasks/task-1.js
+++ JavaScript-UI-and-DOM/05. HTML Templates/tasks/task-1.js
@@ -1,19 +1,29 @@
 /* globals $ */
 
 function solve() {
-  
+
   return function (selector) {
-    var template = ''; /* insert the template here as a string
-        example:
-        var template =
-          '<ul>' +
-            '{{#students}}' +
-            '<li>' +
-              '{{name}}' +
-            '</li>' +
-            '{{/students}}' +
-          '</ul>';
-    */
+    var template =
+      '<table class = "items-table"> ' +
+         '<thead>' +
+           '<tr>' +
+                '<th>#</th>'+
+                '{{#each headers}} <th>{{this}}</th> {{/each}}'+
+           '</tr>' +
+          '</thead>' +
+          '<tbody>'+
+                '{{#each items}}'+
+                  '<tr>'+
+                    '<td>{{@index}}</td>'+
+                    '<td>{{this.col1}}</td>'+
+                    '<td>{{this.col2}}</td>'+
+                    '<td>{{this.col3}}</td>'+                    
+                  '</tr>'+
+                '{{/each}}'+
+
+          '</tbody>'+
+      '</table>'
+      ;
 
     $(selector).html(template);
   };
