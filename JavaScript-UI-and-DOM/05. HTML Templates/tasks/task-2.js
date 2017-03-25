f2ac9f11688c7a37796dd1675df7dc49c5346a34
Milena Shishkova <shishkova.milena@gmail.com>
2017-03-25 15:02:04 +0200

    Added task2 and sample html for it.

--- JavaScript-UI-and-DOM/05. HTML Templates/tasks/task-2.js
+++ JavaScript-UI-and-DOM/05. HTML Templates/tasks/task-2.js
@@ -3,18 +3,24 @@
 function solve() {
   
   return function (selector) {
-    var template = ''; /* insert the template here as a string
-        example:
-        var template =
-          '<ul>' +
-            '{{#students}}' +
-            '<li>' +
-              '{{name}}' +
-            '</li>' +
-            '{{/students}}' +
-          '</ul>';
-    */
-
+    var template = 
+    '<div class="container">'+
+      '<h1>Animals</h1>'+
+        '<ul class="animals-list">'+
+          '{{#each animals}}'+
+              '<li>'+
+                '{{#if this.url}}'+
+                  '<a href={{this.url}} '+
+                   'See a {{this.name}}'+
+                    '{{else}}'+
+                      '<a href="http://cdn.playbuzz.com/cdn/3170bee8-985c-47bc-bbb5-2bcb41e85fe9/d8aa4750-deef-44ac-83a1-f2b5e6ee029a.jpg">'+
+                      'No link for" {{this.name}}, here is Batman!</a>'+
+                    '{{/if}}'+
+                  '</a>'+
+                '</li>'+
+              '{{/each}}'+
+            '</ul>'+
+        '</div>';
     $(selector).html(template);
   };
 };
