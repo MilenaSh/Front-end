8e7ed035efac5f5814c970b92046218a819d2b5d
Milena Shishkova <shishkova.milena@gmail.com>
2017-05-25 20:50:27 +0300

    Changed classes numbering.

--- Web-Applications-with-Node.js/IMDBDemo/config/constants.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    //connection for mongo db
-    connectionString: "mongodb://localhost/moviesDb",
-    genres: ["fantasy", "horror", "comedy", "action", "sci-fi"],
-    pagesCount: 50
-}
--- Web-Applications-with-Node.js/IMDBDemo/config/mongoose.js
+++
@@ -1,8 +0,0 @@
-/* globals require module */
-'use strict';
-
-const mongoose = require("mongoose");
-
-module.exports = function(connectionString) {
-    mongoose.connect(connectionString);
-};
