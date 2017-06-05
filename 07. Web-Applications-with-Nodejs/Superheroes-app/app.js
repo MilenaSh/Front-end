c2a237891a9810873b636fa9f1d6a6a5b4c82b46
Milena Shishkova <shishkova.milena@gmail.com>
2017-06-05 12:36:45 +0300

    Adding starting files for project

---
+++ 07. Web-Applications-with-Nodejs/Superheroes-app/app.js
@@ -0,0 +1,7 @@
+// Model (db, mongoose)
+//                ^
+//                |
+// Controller (BL, data-layer, routes, controller)
+//                                       |
+//                                       v
+// Viwe (Presentation logic) (Pug)
