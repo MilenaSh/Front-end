d29446d9e0b9c34995ace071d8ea3384dc7b5eaa
Milena Shishkova <shishkova.milena@gmail.com>
2017-06-06 16:13:18 +0300

    App and json modified

--- 07. Web-Applications-with-Nodejs/Superheroes-app/app.js
+++ 07. Web-Applications-with-Nodejs/Superheroes-app/app.js
@@ -5,3 +5,21 @@
 //                                       |
 //                                       v
 // Viwe (Presentation logic) (Pug)
+
+/* globals require console */
+
+const config = require("./config");
+
+const app = require("./config/application");
+const data = require("./data")(config);
+
+data.createSuperhero("Batman", ["Utility belt"], ["Justice league", "Bat family"])
+    .then(() => {
+        console.log('Batman created');
+    });
+
+console.log(data);
+
+require("./routers")(app, data);
+
+app.listen(config.port, () => console.log(`Running at port ${config.port}`));
